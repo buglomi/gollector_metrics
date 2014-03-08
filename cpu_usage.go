b20803bd6dd644652a3a5cc5888a3c568d92059a
Erik Hollensbe <erik+github@hollensbe.org>
2014-03-08 07:36:56 -0800

    initial commit: add cpu_usage routines

---
+++ cpu_usage.go
@@ -0,0 +1,88 @@
+package gollector_metrics
+
+/*
+#include <unistd.h>
+int get_hz(void) {
+  return sysconf(_SC_CLK_TCK);
+}
+*/
+import "C"
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func getJiffies() (jiffies int64, cpus int64, err error) {
+	content, err := ioutil.ReadFile("/proc/stat")
+
+	if err != nil {
+		return 0, 0, fmt.Errorf("While processing the cpu_usage package: %s", err.Error())
+	}
+
+	lines := strings.Split(string(content), "\n")
+
+	for _, line := range lines {
+		if strings.Index(line, "cpu ") == 0 {
+			/* cpu with no number is the aggregate of all of them -- this is what we
+			 * want to parse
+			 */
+			parts := strings.Split(line, " ")
+
+			/* 2 - 11 are the time aggregates */
+			for x := 2; x <= 11; x++ {
+
+				/* 5 is the idle time, which we don't want */
+				if x == 5 {
+					continue
+				}
+
+				/* integer all the things */
+				part, err := strconv.Atoi(parts[x])
+
+				if err != nil {
+					return 0, 0, fmt.Errorf("Could not convert integer from string while processing cpu_usage: %s, Error: %s", parts[x], err.Error())
+				}
+
+				jiffies += int64(part)
+			}
+
+		} else if strings.Index(line, "cpu") == 0 {
+			/* cpu with a number is the specific time -- cheat and use this for the
+			 * processor count since we've already read it
+			 */
+			cpus++
+		}
+	}
+
+	return jiffies, cpus, nil
+}
+
+func getJiffyDiff() (int64, int64, error) {
+	time1, cpus, err := getJiffies()
+
+	if err != nil {
+		return 0, 0, nil
+	}
+
+	time.Sleep(1 * time.Second)
+	time2, _, err := getJiffies()
+
+	if err != nil {
+		return 0, 0, nil
+	}
+
+	return time2 - time1, cpus, nil
+}
+
+func CPUUsage() ([2]float64, error) {
+	diff, cpus, err := getJiffyDiff()
+
+	if err != nil {
+		return [2]float64{0, 0}, err
+	}
+	return [2]float64{(float64(diff) / float64(C.get_hz())), float64(cpus)}, nil
+}
