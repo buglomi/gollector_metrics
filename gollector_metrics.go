d46b44ab0f21d8dc0a5e61c6436bb9e0de1008b3
Erik Hollensbe <erik+github@hollensbe.org>
2014-03-08 08:01:58 -0800

    top level documentation.

---
+++ gollector_metrics.go
@@ -0,0 +1,8 @@
+/*
+ A package for metrics collection and reporting.
+
+ Most calls read /proc and are Linux-only (for now, at least). Some call
+ POSIX functions to collect data, so you need a C compiler to build this
+ package.
+*/
+package gollector_metrics
