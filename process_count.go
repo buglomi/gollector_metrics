8c1e769c37373bb9226237faaff73073aff6319a
Erik Hollensbe <erik+github@hollensbe.org>
2014-03-09 08:20:03 -0700

    docs for ProcessCount

--- process_count.go
+++ process_count.go
@@ -1,5 +1,9 @@
 package gollector_metrics
 
+/*
+Count the number of processes running with the given commandline. Returns int,
+error. Count will be zero on any error.
+*/
 func ProcessCount(command string) (int, error) {
 	pids, err := GetPids(command)
 
